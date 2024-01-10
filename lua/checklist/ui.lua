7bfac0b0988f3722baf3c384a7dc8fa2eec07568
swims_hjkl <sumedhrao7@gmail.com>
2024-01-10 07:15:37 +0530

    delete functionality

--- lua/checklist/ui.lua
+++ lua/checklist/ui.lua
@@ -10,6 +10,8 @@ local create_buffer = function()
 		{ noremap = true })
 	vim.api.nvim_buf_set_keymap(buffernr, "n", "q", "<cmd>lua require('checklist.ui').close_window()<cr>",
 		{ noremap = true })
+	vim.api.nvim_buf_set_keymap(buffernr, "n", "d", "<cmd>lua require('checklist.ui').delete_item()<cr>",
+		{ noremap = true })
 	vim.api.nvim_buf_set_option(buffernr, "buftype", "nofile")
 	vim.api.nvim_buf_set_option(buffernr, "swapfile", false)
 	vim.api.nvim_buf_set_option(buffernr, "bufhidden", "delete")
@@ -18,19 +20,15 @@ local create_buffer = function()
 	return buffernr
 end
 
-local save_data = function()
-	print("executing leave funtion")
-	checklist_operations.save_todos_to_storage(M.data_path)
-end
 
 M.close_window = function()
 	vim.api.nvim_win_close(UIWindowId, true)
-	save_data()
+	M.save_data()
 end
+
 
 M.toggle_completed = function()
 	local line_number = vim.fn.line('.')
-	print(line_number)
 	local checklist_item = M.display_data.todo_list[line_number - 2]
 	checklist_item.completed = not checklist_item.completed
 	vim.api.nvim_buf_set_option(UIBufferId, "modifiable", true)
@@ -47,8 +45,8 @@ M.toggle_completed = function()
 end
 
 local populate_ui = function()
-	PT(M.display_data)
 	vim.api.nvim_buf_set_option(UIBufferId, "modifiable", true)
+	vim.api.nvim_buf_set_lines(UIBufferId, 0, -1, false, {})
 	local col_width = vim.fn.winwidth(UIWindowId)
 	print("col_width", col_width)
 	local header_start_col = math.floor(col_width / 2) - math.floor(#M.display_data.display_filter_criteria / 2) - 1
@@ -69,6 +67,19 @@ local populate_ui = function()
 		end
 	end
 	vim.api.nvim_buf_set_option(UIBufferId, "modifiable", false)
+end
+
+M.delete_item = function()
+	local delete_confirmation = vim.fn.input("are you sure you want to delete item? (y/n): ")
+	print(string.lower(delete_confirmation))
+	if (string.lower(delete_confirmation) == "y") then
+		local line_number = vim.fn.line('.')
+		local checklist_item = M.display_data.todo_list[line_number - 2]
+		checklist_operations.delete_todo(checklist_item.id)
+		vim.api.nvim_buf_set_option(UIBufferId, "modifiable", true)
+		vim.api.nvim_buf_set_lines(UIBufferId, line_number - 1, line_number, false, {})
+		vim.api.nvim_buf_set_option(UIBufferId, "modifiable", false)
+	end
 end
 
 local create_window = function(buffernr, start_line, start_col)
